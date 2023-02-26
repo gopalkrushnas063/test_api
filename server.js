acd52a6a1181b83d029e64f6abe4c70372621401
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-02-26 08:59:57 +0530

    test api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname,'db.json'));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
@@ -8,5 +8,5 @@ const path = require('path');
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-server.use('/api',router);
+server.use(router);
 server.listen(PORT,()=> console.log(`JSON Server is running on port${PORT}`));
